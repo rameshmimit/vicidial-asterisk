0a6c86fed5576f4c9ccfb4c001d3a20db0288857
Ramesh Kumar <ramesh@slideshare.com>
2013-12-18 00:07:46 +0530

    Adding move recodings related ruby scripts

---
+++ move_recordings_datewise.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#Copyright 2010 Lito Manansala avmanansala@gmail.com License GPL v2
+#
+#
+#
+require 'rubygems'
+require 'db_conn.rb'
+require 'active_support'
+
+num_of_days = 130
+base_dir = "/var/spool/asterisk/monitor/ORIG"
+dest_dir = "/var/spool/asterisk/monitor/MP3"
+
+puts "Creating Directories..."
+for i in 1..num_of_days
+  dir_to_create = i.days.ago.strftime("%b-%d-%Y")
+  puts "Creating  \"#{dest_dir}/#{dir_to_create}\" directory"
+  exec "mkdir #{dest_dir}/#{dir_to_create}"
+end
+puts "done creating directories..."
---
+++ move_recordings_to.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+#Move recording files to a seperate DATE directory, this is usefull if your directory is growing fast.
+#Copyright 2010 Lito Manansala avmanansala@gmail.com License GPL v2
+#sample usage: ./move_recordings_to.irb 20100219
+#Date Sample format 20100219 for Feb 19, 2010
+
+require 'ftools'
+dateq = ARGV[0]
+basedir = '/var/spool/asterisk/monitorDONE/MP3'
+`mkdir -p "#{basedir}/#{dateq}"`
+#{}`/usr/bin/find "#{basedir}"/ -maxdepth 1 -iname '*"#{dateq}"*' -type f  | xargs -i mv -f {} "#{dateq}"/`
+move_command = "/usr/bin/find #{basedir}/ -maxdepth 1 -iname '*#{dateq}*' -type f  | xargs -i mv -f {} #{dateq}/"
+exec move_command
+puts "#{move_command} .. executed..."
